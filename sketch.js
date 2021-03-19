37a97ecfed9ea731d0c2fa85e28e4f3abce6e977
DeveloperBigsur890 <80718438+DeveloperBigsur890@users.noreply.github.com>
2021-03-19 11:00:00 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,58 +1,130 @@
 var towerImg, tower;
-
 var doorImg, door, doorsGroup;
-
-
 var climberImg, climber, climbersGroup;
-
-
 var ghost, ghostImg;
-
-
 var invisibleBlockGroup, invisibleBlock;
-
-
 var gameState = "play"
 
-//loads all the images used in the game
+
+
 function preload(){
-  
   towerImg = loadImage("tower.png");
   doorImg = loadImage("door.png");
   climberImg = loadImage("climber.png");
   ghostImg = loadImage("ghost-standing.png");
   spookySound = loadSound("spooky.wav");
-  
-  
 }
-
 
 function setup(){
-  
   createCanvas(600,600);
-tower = createSprite(300,300);
-tower.addImage(towerImg);
-tower.velocityY = 1
+  spookySound.loop(); 
+  
+  //background tower
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  
+  //door to be added
+  doorsGroup = new Group();
+  
+  //railing
+  climbersGroup = new Group();
+  invisibleBlockGroup = new Group();
+  
+  
+  //ghost sprite
   ghost = createSprite(200,200,50,50);
-  ghost.scale = 0.3
-  ghost.addImage(ghostImg);
+  ghost.scale = 0.3;
+  ghost.addImage("ghost", ghostImg);
 }
-
 
 function draw(){
   
-  //clear the background
-  background(0);
-if(keyDown("left_arrow")){ 
-   ghost.x=ghost.x-3;}       
-if(keyDown("right_arrow")){ 
-   ghost.x=ghost.x+3;}        
-if(keyDown("space")){ ghost.velocityY = -10; } ghost.velocityY = ghost.velocityY + 0.8
-if(tower.y > 400){
-tower.y = 300
-}  
-drawSprites()  
   
+  
+  background(0);
+  
+  
+  if (gameState === "play") {
+    
+    
+    if(keyDown("left_arrow")){
+      ghost.x = ghost.x - 3;
+    }
+    
+    if(keyDown("right_arrow")){
+      ghost.x = ghost.x + 3;
+    }
+    
+    if(keyDown("space")){
+      ghost.velocityY = -10;
+    }
+    
+    ghost.velocityY = ghost.velocityY + 0.8
+    
+    if(tower.y > 400){
+      tower.y = 300
+    }
+    spawnDoors();
 
+    
+    //climbersGroup.collide(ghost);
+    if(climbersGroup.isTouching(ghost)){
+      ghost.velocityY = 0;
+    }
+    if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
+      ghost.destroy();
+      gameState = "end"
+    }
+    
+    drawSprites();
+  }
+  
+  //end displays text as Game Over!
+  if (gameState === "end"){
+    stroke("yellow");
+    fill("yellow");
+    textSize(30);
+    text("Game Over!!!", 230,250)
+  }
+
+}
+
+function spawnDoors() {
+  //write code here to spawn the doors in the tower
+  if (frameCount % 240 === 0) {
+    
+    var door = createSprite(200, -50);
+    var climber = createSprite(200,10);
+    var invisibleBlock = createSprite(200,15);
+    invisibleBlock.width = climber.width;
+    invisibleBlock.height = 2;
+    
+    door.x = Math.round(random(120,400));
+    climber.x = door.x;
+    invisibleBlock.x = door.x;
+    
+    door.addImage(doorImg);
+    climber.addImage(climberImg);
+    
+    door.velocityY = 1;
+    climber.velocityY = 1;
+    invisibleBlock.velocityY = 1;
+    
+    ghost.depth = door.depth;
+    ghost.depth +=1;
+   
+    //assign lifetime to the variable
+    door.lifetime = 800;
+    climber.lifetime = 800;
+    invisibleBlock.lifetime = 800;
+
+    
+    //add each door to the group
+    doorsGroup.add(door);
+    invisibleBlock.debug = true;
+    climbersGroup.add(climber);
+    invisibleBlockGroup.add(invisibleBlock);
+  }
 }
 
